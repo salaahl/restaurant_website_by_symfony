27917265e6a20a73fc245947329be67b200b015f
salaahl <sokhona.salaha@gmail.com>
2023-02-05 02:55:53 +0100

    Modifications mineures

--- public/js/ajax/reservation.js
+++ public/js/ajax/reservation.js
@@ -28,6 +28,7 @@ $(function () {
         if (data != '') {
           // Mettre dans l'objet "data" l'heure et le jour et les injecter dans le lien présent dans "hour"
           let availablity = data;
+          console.log(availablity);
           availablity.forEach((hour) => {
             $('.availablity').append(
               '<div class="hour"><a href="./complete_reservation.html?">' +
--- public/js/script/navbar.js
+++ public/js/script/navbar.js
@@ -1,26 +1,26 @@
 let main = $('main');
+let errorMargin = 2;
 
-$('.global-container').scroll(function() {
+$('.global-container').scroll(function () {
   let currentScrollPos = $('#scroll-position').position();
-  
-  if ((currentScrollPos.top + 0.6) > scrollPos.top) {
-    navbar.removeClass("navbar-scroll");
-    navbar.addClass("navbar-no-scroll");
-  }
-  else {
-    navbar.removeClass("navbar-no-scroll");
-    navbar.addClass("navbar-scroll");
+
+  if (currentScrollPos.top + errorMargin > scrollPos.top) {
+    navbar.removeClass('navbar-scroll');
+    navbar.addClass('navbar-no-scroll');
+  } else {
+    navbar.removeClass('navbar-no-scroll');
+    navbar.addClass('navbar-scroll');
   }
 });
 
-$( '#nav-toggler-icon' ).click(function() {
-  if($('.navbar-collapse').hasClass('show')) {
+$('#nav-toggler-icon').click(function () {
+  if ($('.navbar-collapse').hasClass('show')) {
     navbar.children('.navbar-collapse').removeClass('show');
-    navbar.removeClass("black-navbar");
-    main.removeClass("main-margin");
+    navbar.removeClass('black-navbar');
+    main.removeClass('main-margin');
   } else {
     navbar.children('.navbar-collapse').addClass('show');
-    navbar.addClass("black-navbar");
-    main.addClass("main-margin");
+    navbar.addClass('black-navbar');
+    main.addClass('main-margin');
   }
 });
