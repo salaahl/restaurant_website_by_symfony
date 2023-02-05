f484a055ece8227a3b4e0d7af2ae1717fa70d5ee
salaahl <sokhona.salaha@gmail.com>
2023-02-05 03:25:28 +0100

    Correctif au niveau du traitement de la réponse

--- public/js/ajax/reservation.js
+++ public/js/ajax/reservation.js
@@ -27,8 +27,7 @@ $(function () {
       success: function (data) {
         if (data != '') {
           // Mettre dans l'objet "data" l'heure et le jour et les injecter dans le lien présent dans "hour"
-          let availablity = data;
-          console.log(availablity);
+          let availablity = JSON.parse(data);
           availablity.forEach((hour) => {
             $('.availablity').append(
               '<div class="hour"><a href="./complete_reservation.html?">' +
