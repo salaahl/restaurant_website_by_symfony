1a3a5d240abcde990d3d016492971a48d8f6b96d
Salaha Sokhona <67805036+salaahl@users.noreply.github.com>
2024-05-31 16:51:07 +0200

    Update MenuController.php

--- src/Controller/MenuController.php
+++ src/Controller/MenuController.php
@@ -6,7 +6,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
-use Doctrine\Persistence\ManagerRegistry;
 
 use App\Entity\Menu;
 use App\Form\MenuType;
@@ -14,10 +13,11 @@ use App\Repository\MenuRepository;
 
 class MenuController extends AbstractController
 {
+    protected MenuRepository $menuRepository;
+    
     #[Route('/menu/create', name: 'menu.create')]
     public function create(
         Request $request,
-        MenuRepository $menuRepository
     ): Response {
 
         $menu = new Menu();
@@ -25,7 +25,7 @@ class MenuController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $menuRepository->save($menu, true);
+            $this->menuRepository->save($menu, true);
             return new Response(true);
         }
         
@@ -37,7 +37,6 @@ class MenuController extends AbstractController
     #[Route('/menu/update', name: 'menu.update')]
     public function update(
         Request $request,
-        MenuRepository $menuRepository
     ): Response {
         //
     }
@@ -45,7 +44,6 @@ class MenuController extends AbstractController
     #[Route('/menu/edit', name: 'menu.edit')]
     public function edit(
         Request $request,
-        MenuRepository $menuRepository
     ): Response {
         //
     }
@@ -53,7 +51,6 @@ class MenuController extends AbstractController
     #[Route('/menu/delete', name: 'menu.delete')]
     public function delete(
         Request $request,
-        MenuRepository $menuRepository
     ): Response {
         //
     }
