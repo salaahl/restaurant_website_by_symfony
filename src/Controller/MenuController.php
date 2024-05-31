debaca183e913042f8ee0ad39d54e07e1a2218de
Salaha Sokhona <67805036+salaahl@users.noreply.github.com>
2024-05-31 15:46:54 +0200

    Update MenuController.php

--- src/Controller/MenuController.php
+++ src/Controller/MenuController.php
@@ -5,23 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\Persistence\ManagerRegistry;
 
-use App\Entity\Dish;
 use App\Entity\Menu;
-
-use App\Form\DishType;
 use App\Form\MenuType;
-
-use App\Repository\DishRepository;
 use App\Repository\MenuRepository;
 
 class MenuController extends AbstractController
 {
-    #[Route('/add_menu', name: 'app_add_menu')]
-    public function addMenu(
+    #[Route('/menu/create', name: 'menu.create')]
+    public function create(
         Request $request,
         ManagerRegistry $doctrine
     ): Response {
@@ -31,43 +25,38 @@ class MenuController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-
             $entityManager = $doctrine->getManager();
             $entityManager->persist($menu);
             $entityManager->flush();
-
             return new Response(true);
         }
-        return $this->render('menu/add_menu.html.twig', [
+        
+        return $this->render('menu/create.html.twig', [
             'form' => $form,
         ]);
     }
 
-    #[Route('/add_dish', name: 'app_add_dish')]
-    public function addDish(
-        MenuRepository $menuRepository,
+    #[Route('/menu/update', name: 'menu.update')]
+    public function update(
         Request $request,
         ManagerRegistry $doctrine
     ): Response {
+        //
+    }
 
-        $dish = new Dish();
-        $menus = $menuRepository->findAll();
-        
-        $form = $this->createForm(DishType::class, $dish);
-        $form->handleRequest($request);
+    #[Route('/menu/edit', name: 'menu.edit')]
+    public function edit(
+        Request $request,
+        ManagerRegistry $doctrine
+    ): Response {
+        //
+    }
 
-        if ($form->isSubmitted() /*&& $form->isValid()*/) {
-
-            $entityManager = $doctrine->getManager();
-            $entityManager->persist($dish);
-            $entityManager->flush();
-
-            return new Response(true);
-        }
-
-        return $this->render('menu/add_dish.html.twig', [
-            'form' => $form,
-            'menus' => $menus,
-        ]);
+    #[Route('/menu/delete', name: 'menu.delete')]
+    public function delete(
+        Request $request,
+        ManagerRegistry $doctrine
+    ): Response {
+        //
     }
 }
