3ef22031e6c74fc854105312b2a1c8555bdb962f
salaahl <sokhona.salaha@gmail.com>
2024-08-20 23:57:18 +0200

    ajout des formulaires

---
+++ src/Form/ReservationDateType.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\ReservationDate;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ReservationDateType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('reservation_date')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => ReservationDate::class,
+        ]);
+    }
+}
---
+++ src/Form/ReservationType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Reservation;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ReservationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('mail')
+            ->add('surname')
+            ->add('name')
+            ->add('phone_number')
+            ->add('seat_reserved')
+            ->add('hour')
+            ->add('reservation_date')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Reservation::class,
+        ]);
+    }
+}
