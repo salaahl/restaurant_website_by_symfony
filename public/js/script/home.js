ab2b7113fe01d221e4aada1bfbb4e4648b0e1609
Salaha Sokhona <salah@MacBookPro.lan>
2024-12-11 20:51:17 +0100

    style : ajustements

--- public/js/script/home.js
+++ public/js/script/home.js
@@ -1,57 +1,49 @@
 window.addEventListener("load", () => {
   setTimeout(() => {
     function reduceNavbar() {
-      gsap.to(
-        ".navbar",
-        {
-          minHeight: "6vh",
-          width: window.innerWidth < 1024 ? 700 : 750,
-          borderRadius: 40,
-        });
+      gsap.to(".navbar", {
+        minHeight: "6vh",
+        width: window.innerWidth < 1024 ? 700 : 750,
+        borderRadius: 40,
+      });
 
-      gsap.to(
-        ".navbar-brand",
-        {
-          fontSize: "large"
-        });
+      gsap.to(".navbar-brand", {
+        fontSize: "large",
+      });
     }
-    
+
     function resetNavbar() {
-      gsap.to(
-        ".navbar",
-        {
-          minHeight: "7vh",
-          width: "98%",
-          borderRadius: 25,
-        });
+      gsap.to(".navbar", {
+        minHeight: "7vh",
+        width: "98%",
+        borderRadius: 25,
+      });
 
-      gsap.to(
-        ".navbar-brand",
-        {
-          fontSize: "x-large"
-        });
+      gsap.to(".navbar-brand", {
+        fontSize: "x-large",
+      });
     }
-    
+
     gsap.matchMedia().add("(min-width: 768px)", () => {
       ScrollTrigger.create({
-        trigger: "main",
-        start: "1% 0",
-        end: "1% 0",
+        trigger: "#navbar",
+        start: "0 0",
+        end: "0 0",
         onEnter: () => reduceNavbar(),
         onEnterBack: () => resetNavbar(),
       });
-  
+
       gsap.from("#about-me-container", {
-        y: "50%",
+        y: "100%",
         opacity: 0,
         scrollTrigger: {
           trigger: "#about-me-container",
           start: "0% 100%",
-          end: "50% 100%",
+          end: "100% 100%",
           scrub: true,
         },
       });
-  
+
       gsap.from("#menu-title-container .title", {
         y: "100%",
         opacity: 0,
@@ -63,7 +55,7 @@ window.addEventListener("load", () => {
           scrub: true,
         },
       });
-  
+
       gsap.from(".straight-lines.mirror > .straight", {
         x: "100%",
         opacity: 0,
@@ -77,7 +69,7 @@ window.addEventListener("load", () => {
           scrub: true,
         },
       });
-  
+
       gsap.from(".straight-lines:first-of-type > .straight", {
         x: "-100%",
         opacity: 0,
@@ -88,6 +80,17 @@ window.addEventListener("load", () => {
           trigger: "#menu-title-container .title",
           start: "0% 100%",
           end: "+=50%",
+          scrub: true,
+        },
+      });
+
+      gsap.from("#reservation-container", {
+        y: "100%",
+        opacity: 0,
+        scrollTrigger: {
+          trigger: "#reservation-container",
+          start: "0% 100%",
+          end: "100% 100%",
           scrub: true,
         },
       });
--- public/js/script/navbar-scroll.js
+++ public/js/script/navbar-scroll.js
@@ -2,19 +2,21 @@ window.addEventListener("load", () => {
   // L'animation est gérée autrement sur la page d'accueil
   if (window.location.pathname !== "/") {
     gsap.matchMedia().add("(min-width: 768px)", () => {
-      gsap.fromTo(".navbar", 
-      {
-        backgroundColor: "transparent",
-      },
-      {
-        backgroundColor: "white",
-        scrollTrigger: {
-          trigger: "html",
-          start: "0 0",
-          end: "15% 0",
-          scrub: true,
+      gsap.fromTo(
+        ".navbar",
+        {
+          backgroundColor: "transparent",
         },
-      });
+        {
+          backgroundColor: "white",
+          scrollTrigger: {
+            trigger: "html",
+            start: "0 0",
+            end: "15% 0",
+            scrub: true,
+          },
+        }
+      );
     });
   }
 });
