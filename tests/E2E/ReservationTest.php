22b17732b1eff5c17f4ccb9e6187597e7f9d3346
Salaha Sokhona <67805036+salaahl@users.noreply.github.com>
2024-12-19 14:27:21 +0100

    Update ReservationTest.php

--- tests/E2E/ReservationTest.php
+++ tests/E2E/ReservationTest.php
@@ -5,6 +5,7 @@ namespace App\Tests;
 use Symfony\Component\Panther\PantherTestCase;
 use Facebook\WebDriver\WebDriverBy;
 use Doctrine\ORM\EntityManagerInterface;
+
 use App\Entity\Reservation;
 
 // E2E ou les tests de "bout en bout" simulent des scénarios réels d'utilisation, en interagissant avec l'application comme un utilisateur final
@@ -14,6 +15,7 @@ class ReservationTest extends PantherTestCase
     {
         $client = static::createPantherClient();
         $client->request('GET', '/reservation');
+        $date = new \DateTime('now', new \DateTimeZone('Europe/Paris'));
 
         // Trouver le bouton par son sélecteur CSS et cliquer dessus
         $client->findElement(WebDriverBy::cssSelector('#new-reservation-button'))->click();
@@ -21,7 +23,7 @@ class ReservationTest extends PantherTestCase
         $client->waitFor('#new-reservation');
         $client->submitForm('Voir les disponibilités', [
             'seats' => 2,
-            'date' => date('d/m/Y')
+            'date' => $date->format('d/m/Y')
         ]);
 
         $client->waitFor('#new-reservation .hour');
@@ -36,12 +38,11 @@ class ReservationTest extends PantherTestCase
         ]);
 
         // Vérifier que la redirection se fait vers la page de confirmation
-        $this->assertEquals('/reservation', parse_url($client->getCurrentURL())['path']);
-
-        $client->waitFor('.reservation-confirmed');
+        $this->assertEquals('/reservation', parse_url($client->getCurrentURL())['path'], 'Erreur lors de la redirection vers la page de confirmation');
 
         // Vérifier que la page de confirmation contient le bon contenu
-        $this->assertSelectorTextContains('p', 'Votre réservation du ' . date('Y-m-d') . ' à ' . $hour . ' a bien été prise en compte.');
+        $client->waitFor('.reservation-confirmed');
+        $this->assertSelectorTextContains('p', sprintf('Votre réservation du %s à %s a bien été prise en compte.', ($date->format('Y-m-d'), $hour), 'Erreur lors de l'affichage de la page de confirmation');
     }
 
     public function testGetReservation(): void
@@ -53,23 +54,25 @@ class ReservationTest extends PantherTestCase
         // Repositories
         $reservationRepository = $manager->getRepository(Reservation::class);
         $user = $reservationRepository->find(4);
-        $this->assertNotNull($user);
+        $this->assertNotNull($user, 'Aucune réservation n'est associée à cet ID');
 
         // Effectuer une requête GET sur la page de réservation
         $client = static::createPantherClient();
         $crawler = $client->request('GET', '/reservation');
 
         // Trouver le bouton par son sélecteur CSS et cliquer dessus
-        $button = $client->findElement(WebDriverBy::cssSelector('button#check-reservation-button'));
-        $button->click();
+        $client->waitFor('#check-reservation-button');
+        $client->findElement(WebDriverBy::cssSelector('#check-reservation-button'))->click();
 
         // Remplir le formulaire et le soumettre
-        $client->findElement(WebDriverBy::cssSelector('input[name="mail"]'))->sendKeys($user->getMail());
-        $client->findElement(WebDriverBy::cssSelector('input[name="surname"]'))->sendKeys($user->getSurname());
-        $client->findElement(WebDriverBy::cssSelector('#check_reservation-form button[type="submit"]'))->click();
+        $client->waitFor('#check_reservation-form');
+        $client->submitForm('Rechercher', [
+            'mail' => $user->getMail(),
+            'surname' => $user->getSurname()
+        ]);
 
         // Vérifier que la page contient le texte attendu
         $client->waitFor('.check-reservation-response');
-        $this->assertSelectorTextContains('.check-reservation-response', $user->getName() . ' ' . $user->getSurname());
+        $this->assertSelectorTextContains('.check-reservation-response', $user->getName() . ' ' . $user->getSurname(), 'Aucune réservation à ce nom');
     }
 }
