6fab4fea5f5d5865dcc84b81dd80d396a1d2153c
Salaha Sokhona <67805036+salaahl@users.noreply.github.com>
2025-04-23 17:21:32 +0200

    Update Version20241224050242.php
    passage à postgres

--- migrations/Version20241224050242.php
+++ migrations/Version20241224050242.php
@@ -19,16 +19,69 @@ final class Version20241224050242 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE dish (id SERIAL NOT NULL, menu_id INT NOT NULL, name VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, price DOUBLE PRECISION NOT NULL, INDEX IDX_957D8CB8CCD7E912 (menu_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE menu (id SERIAL NOT NULL, name VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_7D053A935E237E06 (name), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE reservation (id SERIAL NOT NULL, date_id INT NOT NULL, email VARCHAR(255) NOT NULL, surname VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, phone_number VARCHAR(255) DEFAULT NULL, seats INT NOT NULL, hour DOUBLE PRECISION NOT NULL, INDEX IDX_42C84955B897366B (date_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE reservation_date (id SERIAL NOT NULL, date DATETIME NOT NULL, UNIQUE INDEX UNIQ_BCA7FA12AA9E377A (date), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE seat (id SERIAL NOT NULL, reservation_date_id INT NOT NULL, hour DOUBLE PRECISION NOT NULL, seats_available INT NOT NULL, INDEX IDX_3D5C3666DF028DEE (reservation_date_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE messenger_messages (id BIGSERIAL NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE dish ADD CONSTRAINT FK_957D8CB8CCD7E912 FOREIGN KEY (menu_id) REFERENCES menu (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE reservation ADD CONSTRAINT FK_42C84955B897366B FOREIGN KEY (date_id) REFERENCES reservation_date (id)');
-        $this->addSql('ALTER TABLE seat ADD CONSTRAINT FK_3D5C3666DF028DEE FOREIGN KEY (reservation_date_id) REFERENCES reservation_date (id)');
+        $this->addSql('
+            CREATE TABLE menu (
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(255) NOT NULL UNIQUE
+            )
+        ');
+    
+        $this->addSql('
+            CREATE TABLE dish (
+                id SERIAL PRIMARY KEY,
+                menu_id INTEGER NOT NULL,
+                name VARCHAR(255) NOT NULL,
+                description TEXT NOT NULL,
+                price DOUBLE PRECISION NOT NULL,
+                CONSTRAINT FK_957D8CB8CCD7E912 FOREIGN KEY (menu_id) REFERENCES menu (id) ON DELETE CASCADE
+            )
+        ');
+    
+        $this->addSql('
+            CREATE TABLE reservation_date (
+                id SERIAL PRIMARY KEY,
+                date TIMESTAMP NOT NULL UNIQUE
+            )
+        ');
+    
+        $this->addSql('
+            CREATE TABLE reservation (
+                id SERIAL PRIMARY KEY,
+                date_id INTEGER NOT NULL,
+                email VARCHAR(255) NOT NULL,
+                surname VARCHAR(255) NOT NULL,
+                name VARCHAR(255) NOT NULL,
+                phone_number VARCHAR(255),
+                seats INTEGER NOT NULL,
+                hour DOUBLE PRECISION NOT NULL,
+                CONSTRAINT FK_42C84955B897366B FOREIGN KEY (date_id) REFERENCES reservation_date (id)
+            )
+        ');
+    
+        $this->addSql('
+            CREATE TABLE seat (
+                id SERIAL PRIMARY KEY,
+                reservation_date_id INTEGER NOT NULL,
+                hour DOUBLE PRECISION NOT NULL,
+                seats_available INTEGER NOT NULL,
+                CONSTRAINT FK_3D5C3666DF028DEE FOREIGN KEY (reservation_date_id) REFERENCES reservation_date (id)
+            )
+        ');
+    
+        $this->addSql('
+            CREATE TABLE messenger_messages (
+                id BIGSERIAL PRIMARY KEY,
+                body TEXT NOT NULL,
+                headers TEXT NOT NULL,
+                queue_name VARCHAR(190) NOT NULL,
+                created_at TIMESTAMP NOT NULL,
+                available_at TIMESTAMP NOT NULL,
+                delivered_at TIMESTAMP DEFAULT NULL
+            )
+        ');
+        $this->addSql('CREATE INDEX IDX_75EA56E0FB7336F0 ON messenger_messages (queue_name)');
+        $this->addSql('CREATE INDEX IDX_75EA56E0E3BD61CE ON messenger_messages (available_at)');
+        $this->addSql('CREATE INDEX IDX_75EA56E016BA31DB ON messenger_messages (delivered_at)');
     }
 
     public function down(Schema $schema): void
