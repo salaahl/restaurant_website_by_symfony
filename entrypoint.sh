62ea55cd1e5d80df12cb9db286635ecb093442bc
salaahl <sokhona.salaha@gmail.com>
2025-07-20 03:09:47 +0200

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,9 @@ echo "Préparation du cache Symfony..."
 php bin/console cache:clear --env=prod --no-debug
 php bin/console cache:warmup --env=prod --no-debug
 
-echo "Migrations..."
+echo "Création de la base de données et lancement des migrations..."
+php bin/console doctrine:database:drop --force || true
+php bin/console doctrine:database:create
 php bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration
 
 exec "$@"
